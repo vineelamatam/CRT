6c3afbda05001a98af2435cb3a3ebd332f748aee
vineela.matam <matvineela655@gmail.com>
2026-01-19 10:02:12 +0530

    CRT

---
+++ IV-SEM/python/practice/M01_Lnguge_and_progrmming_Basics/S01/PS01_python_setup_and_syntax.py
@@ -0,0 +1,5 @@
+#pratice.problem: python setup & syntax
+user_name12 = "vinni 1"
+print(user_name12)
+and12 = 100
+print(and12)
---
+++ IV-SEM/python/practice/M01_Lnguge_and_progrmming_Basics/S01/PSo2_Variable_and_data_types.py
@@ -0,0 +1,21 @@
+# Practice Problem : Variables & data types
+'''
+Data type:
+1. Fundamental DT:
+    1. int
+    2. float
+    3. complex
+    4. Boolean
+    5. String
+
+2. Collection DT:
+    1. int
+    2. Tuple
+    3. set
+    4. Dictionary
+'''
+
+x = 10
+y = 12e3
+print(x,type(x))
+print(y,type(y))
