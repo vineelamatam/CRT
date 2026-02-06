d8962b80108c68e68f4bf5e160c70b1bd11be971
vineela.matam <matvineela655@gmail.com>
2026-02-06 14:30:03 +0530

    module 2

---
+++ IV-SEM/python/practice/M01_Lnguge_and_progrmming_Basics/S02/PS03_operators.py
@@ -0,0 +1,12 @@
+x = 7
+y = 13
+print(x & y)
+print(x | y)
+print(x ^ y)
+print(~x)
+print(x << 2)
+print(x >> 2)
+
+#Membership Operators
+print(100 in [10,20, 30, 40, 50])
+print("on" in "python ")
---
+++ IV-SEM/python/practice/M01_Lnguge_and_progrmming_Basics/S02/pS04_conditional_statements.py
@@ -0,0 +1,12 @@
+# Read a number from the userand check if is +ve, -ve or zero
+'''
+Input = 10
+output = '+ve'
+
+Input = 0
+output = 'zero'
+
+Input = -5
+output= '-ve'
+
+'''
